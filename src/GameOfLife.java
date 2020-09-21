d65eb9a3c10378a3bd457a374b2ab585cc288e87
Robert Ardeleanu <robert.ardeleanu@gmx.at>
2020-09-21 10:07:57 +0200

    new

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -10,24 +10,25 @@ public class GameOfLife {
 
         System.out.println("Type in the amount of generations you want to go through!");
         int generation = scan.nextInt();
-        int gridWidth = 25, gridLength = gridWidth;
+
+        int gridWidthLength = 25;
 
         //two empty 2D arrays
-        int[][] grid = new int[gridLength - 1][gridWidth - 1];
-        int[][] gridNew = new int[gridLength - 1][gridWidth - 1];
+        int[][] grid = new int[gridWidthLength][gridWidthLength];
+        int[][] gridNew = new int[gridWidthLength][gridWidthLength];
 
         //loop for the generation
-        for (int k = 0; k < generation; k++) {
-            int gene = 0;
-            gene++;
-            System.out.println("Generation: " + gene);
+        for (int k = 0; k <= generation; k++) {
+            System.out.println();
+            System.out.println("Next generation:");
             //loop row
-            for (int i = 0; i < grid.length; i++) {
+            for (int i = 1; i < grid.length-1; i++) {
                 System.out.println();
                 //loop col
-                for (int j = 0; j < grid.length; j++) {
+                for (int j = 1; j < grid[i].length-1; j++) {
                     //first gen random
                     if (k == 0) {
+
                         grid[i][j] = rnd.nextInt(2);
                         if (grid[i][j] == 1) {
                             System.out.print(" * ");
@@ -36,25 +37,31 @@ public class GameOfLife {
                         }
                         //further gens not rnd
                     } else {
+                        if (grid[i][j] == 1) {
+                            System.out.print(" * ");
+                        } else {
+                            System.out.print(" . ");
+                        }
 
                         int livingNeighbourCells = 0;
 
                         for (int l = -1; l <= 1; l++) {
                             for (int m = -1; m <= 1; m++) {
                                 livingNeighbourCells += grid[i + l][j + m];
-                                livingNeighbourCells -= grid[l][m];
-                                if (grid[l][m] == 0 && livingNeighbourCells == 3) {
-                                    gridNew[l][m] = 1;
-                                } else if (grid[l][m] == 1 && livingNeighbourCells < 2) {
-                                    gridNew[l][m] = 0;
-                                } else if (grid[l][m] == 1 && (livingNeighbourCells == 2 || livingNeighbourCells == 3)) {
-                                    gridNew[l][m] = 1;
-                                } else if (grid[l][m] == 1 && livingNeighbourCells > 3) {
-                                    gridNew[l][m] = 0;
+                                livingNeighbourCells -= grid[i][j];
+
+                                if (grid[i][j] == 0 && livingNeighbourCells == 3) {
+                                    gridNew[i][j] = 1;
+                                } else if (grid[i][j] == 1 && livingNeighbourCells < 2) {
+                                    gridNew[i][j] = 0;
+                                } else if (grid[i][j] == 1 && (livingNeighbourCells == 2 || livingNeighbourCells == 3)) {
+                                    gridNew[i][j] = 1;
+                                } else if (grid[i][j] == 1 && livingNeighbourCells > 3) {
+                                    gridNew[i][j] = 0;
                                 } else {
-                                    gridNew[l][m] = grid[l][m];
+                                    gridNew[i][j] = grid[i][j];
                                 }
-                                grid[l][m] = gridNew[l][m];
+                               grid[i][j] = gridNew[i][j];
                             }
                         }
 
